a4b121171007052281dd3fc6fffad03258ea5259
Tarun Teja <renkelatarunteja@gmail.com>
2024-03-01 17:18:41 +0530

    changes are made

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import XModal from './XModal'; // Assuming XModal component is in a separate fil
 function App() {
   return (
     <div className="App">
-      <h1>XModal Application</h1>
+      <h1>User Details Modal</h1>
       <XModal />
     </div>
   );
